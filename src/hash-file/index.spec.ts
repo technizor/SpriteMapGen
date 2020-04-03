0f9e992805f59379b41aa4ed50bfb0b6011bc172
Sherman Ying <fishermanying@gmail.com>
2020-04-03 00:40:49 -0400

    Fix istanbul coverage including spec files

--- src/hash-file/index.spec.ts
+++ src/hash-file/index.spec.ts
@@ -2,21 +2,21 @@ import { expect } from 'chai';
 
 import hashFile from '.';
 
-describe('hashFile', function () {
+describe('hashFile function', function () {
     it('is a function', function () {
-        let func = hashFile(true);
+        let func = hashFile(false);
         expect(func).to.be.not.null;
     });
     it('returns null for non-existent file at path', async function () {
-        let func = hashFile(true);
+        let func = hashFile(false);
         expect(await func('nothing.json')).to.be.null;
     });
     it('throws for existing folder at path', async function () {
-        let func = hashFile(true);
+        let func = hashFile(false);
         expect(await func('resources')).to.be.null;
     });
     it('returns sha256 hash for existing file at path', async function () {
-        let func = hashFile(true);
+        let func = hashFile(false);
         expect(await func('resources/example-map.json')).to.be.not.null;
     });
 });
